67c0f47d43f46b8fd0936b22be67632d62789473
Jimena <jimenanrp2@gmail.com>
2023-02-27 01:02:35 +0100

    Add models file

---
+++ src/models/dog.model.tsx
@@ -0,0 +1,14 @@
+export type ProtoDogStructure = {
+  name: string;
+  breed: string;
+  weight: number;
+  is_good: boolean;
+};
+
+export type DogStructure = {
+  id: number;
+  name: string;
+  breed: string;
+  weight: number;
+  is_good: boolean;
+};

d27fdd6d302b1a8640431829f35d9646c43b216e
Jimena <jimenanrp2@gmail.com>
2023-02-27 07:51:32 +0100

    Delete unused imports

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,12 @@
-import { useMemo } from "react";
 import { Link } from "react-router-dom";
-import { useDogs } from "../../hook/use.dogs";
-import { DogApiRepo } from "../../services/dog.api.repo";
 
 export function Header() {
-
   return (
     <>
       <h1>PERRITOS</h1>
       <nav>
         <ul>
-          <Link to={`/detail/add`}></Link>
+          {/* <Link to={`/detail/add`}></Link> */}
           <li>Crear un nuevo perrito</li>
         </ul>
       </nav>

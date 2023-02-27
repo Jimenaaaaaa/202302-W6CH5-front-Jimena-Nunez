595ad868852f2b23ab64a4278c5fa06eebec9ea8
Jimena <jimenanrp2@gmail.com>
2023-02-27 01:02:28 +0100

    Create files

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+export function Header() {
+  return (
+    <>
+      <h1>PERRITOS</h1>
+      <nav>
+        <ul>
+          <li>Crear un nuevo perrito</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
